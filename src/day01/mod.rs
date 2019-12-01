e14b16fe5aac3530e2ad3004426f6f94e734523d
Johan Horvius <drhorv@gmail.com>
2019-12-01 14:00:45 +0100

    Move calculate fuel outside of struct

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -3,11 +3,9 @@ use crate::lib::Solver;
 
 pub(crate) struct Day1Solver {}
 
-impl Day1Solver {
-    fn calculate_fuel(&self, weight: i32) -> i32 {
+fn calculate_fuel(weight: i32) -> i32 {
         (weight as f64 / 3.0) as i32 - 2
     }
-}
 
 impl Solver for Day1Solver {
     fn solve(&self, lines: Vec<String>, part_two: bool) -> String {
@@ -15,16 +13,16 @@ impl Solver for Day1Solver {
             return lines
                 .into_iter()
                 .map(|s| s.parse::<i32>().unwrap())
-                .map(|i| self.calculate_fuel(i))
+                .map(|i| calculate_fuel(i))
                 .sum::<i32>().to_string()
         }
 
         let mut total = 0;
         for weight in lines.iter().map(|s| s.parse::<i32>().unwrap()) {
-            let mut fuel_to_add = self.calculate_fuel(weight);
+            let mut fuel_to_add = calculate_fuel(weight);
             while fuel_to_add > 0 {
                 total += fuel_to_add;
-                fuel_to_add = self.calculate_fuel(fuel_to_add);
+                fuel_to_add = calculate_fuel(fuel_to_add);
             }
         }
 
@@ -36,7 +34,6 @@ impl Solver for Day1Solver {
 mod tests {
     use super::*;
     use crate::lib::test_solver;
-
 
     #[test]
     fn test_part_one() {
@@ -50,7 +47,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let solver = Day1Solver{};
-
         test_solver(&solver, true, &["14"], "2");
         test_solver(&solver, true, &["1969"], "966");
         test_solver(&solver, true, &["100756"], "50346");
