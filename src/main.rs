387a4bb1a044562ad86e4119f3956515378f5d0d
Johan Horvius <drhorv@gmail.com>
2019-12-02 08:06:00 +0100

    Solution for day2

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ use lib::{Solver, read_lines};
 
 mod day01;
 use day01::Day1Solver;
+mod day02;
+use day02::Day2Solver;
 
 fn main() {
-    let (solver, file) = (Day1Solver{}, "day01");
+    let (solver, file) = (Day2Solver{}, "src/day02/day02");
 
     let now = Instant::now();
     let answer: String = solver.solve(read_lines(file), true);
